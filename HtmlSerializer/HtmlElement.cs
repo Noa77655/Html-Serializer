688a6dc0498f9878896fccf631d07b7f59267dcc
Noa77655 <n0527677655@gmail.com>
2024-12-14 19:17:20 +0200

    my commit message

---
+++ HtmlSerializer/HtmlElement.cs
@@ -0,0 +1,19 @@
+﻿  public class HtmlElement
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Attributes { get; set; }
+        public List<string> Classes { get; set; }
+        public string InnerHtml { get; set; }
+        public HtmlElement Parent { get; set; }
+        public List<HtmlElement> Children { get; set; }
+
+        public HtmlElement()
+        {
+            Attributes = new List<string>();
+            Classes = new List<string>();
+            Children = new List<HtmlElement>();
+        }
+    }
+
+
